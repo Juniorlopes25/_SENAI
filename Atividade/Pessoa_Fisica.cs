23811417d7b88d8623164dcd29a35e086ee63ba0
Adirson Lopes Junior <juniorlopes02525@gmail.com>
2023-09-04 18:35:42 -0300

    Initial commit

---
+++ Atividade/Pessoa_Fisica.cs
@@ -0,0 +1,10 @@
+using System.Reflection.Metadata;
+
+namespace Atividade
+{
+    class Pessoa_Fisica : Clientes
+    {
+        public String cpf {get; set;}
+        public string rg {get; set;}
+    }
+}
